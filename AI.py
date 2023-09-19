e06ef2c9a00a77217e69ecaa2221fc301016c321
sterben-209 <khangphanhuynh2007@gmail.com>
2023-09-19 22:53:48 +0700

    bat dau lam

--- AI.py
+++ AI.py
@@ -1,3 +1,8 @@
 import mediapipe as mp 
-print("hello")
-print("test lenh pull")
+import cv2 
+
+
+cap = cv2.VideoCapture(0) # chọn cam máy  
+cap.set(3,1080) # set độ rộng 
+cap.set(4,1920) # set độ dài 
+detector = HandDetector(detectionCon = 0.8)
