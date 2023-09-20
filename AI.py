91caf39b9deea4865fd7a5e1f7ec7a2c3c4b6d79
sterben-209 <85734242+sterben-209@users.noreply.github.com>
2023-09-20 08:37:01 +0700

    Update AI.py

--- AI.py
+++ AI.py
@@ -4,6 +4,3 @@ from mediapipe.task import python
 from mediapipe.task.python import vision 
 
 
-cap = cv2.VideoCapture(0) # chọn cam máy  
-cap.set(3,1080) # set độ rộng 
-cap.set(4,1920) # set độ dài 
