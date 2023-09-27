12a1d1c2d1c9caf5f2a14e6d041269453026f5e7
sterben-209 <khangphanhuynh2007@gmail.com>
2023-09-27 23:00:55 +0700

    object-tracking

--- test.py
+++ test.py
@@ -1,23 +1,38 @@
-import cv2
 import mediapipe as mp
+import cv2
 
-cap = cv2.VideoCapture(0)
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
-mpDraw = mp.solutions.drawing_utils
+# Khởi tạo pipeline object detection
+object_detection = mp.solutions.object_detection.ObjectDetection(
+    model_path="models/object_detection/mobilenet_ssd_v2_coco.tflite"
+)
 
-success, img = cap.read()
-imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-results = hands.process(imgRGB)
+# Đọc video
+cap = cv2.VideoCapture("video.mp4")
 
-if results.multi_hand_landmarks:
-    for handLms in results.multi_hand_landmarks:
-        for id, lm in enumerate(handLms.landmark):
-            cx, cy = int(lm.x * w), int(lm.y * h)
-            if id == 0:
-                cv2.circle(img, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
-            else:
-                cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
-        mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+while True:
+    # Đọc khung hiện tại từ video
+    ret, frame = cap.read()
 
-cv2.imshow("test", results)
+    # Nếu không có khung, thoát vòng
+    if not ret:
+        break
+
+    # Chạy pipeline trên khung
+    results = object_detection.process(frame)
+
+    # Vẽ kết quả trên khung
+    for detection in results.detections:
+        cv2.rectangle(frame, detection.bounding_box, (255, 0, 0), 2)
+        cv2.putText(frame, detection.label, detection.bounding_box.center, cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2)
+
+    # Hiển thị khung
+    cv2.imshow("Output", frame)
+
+    # Kiểm tra phím ESC để thoát
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+# Giải phóng tài nguyên
+cap.release()
+cv2.destroyAllWindows()
